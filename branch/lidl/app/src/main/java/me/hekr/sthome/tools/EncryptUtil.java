ec06f7814771200e40367b29e6dae42f2cf072e9
Ryan Hsueh <freestyle0103@gmail.com>
2018-12-11 15:23:56 +0800

    [What] Init AES encrypt fuctions

--- branch/lidl/app/src/main/java/me/hekr/sthome/tools/EncryptUtil.java
+++ branch/lidl/app/src/main/java/me/hekr/sthome/tools/EncryptUtil.java
@@ -1,12 +1,92 @@
 package me.hekr.sthome.tools;
 
+import android.support.annotation.NonNull;
+import android.util.Base64;
+
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Created by ryanhsueh on 2018/12/11
  */
 public class EncryptUtil {
+
+    // Do not modify this if unnecessary
+    private final static String IvAES = "78909876shwmwkbq" ; // 16 bytes
+    private final static String KeyAES = "322jdf9whsdiwqwq921mmekqow10k8dw"; // 32 bytes
+
+    public static String encrypt(@NonNull final String text) {
+        try {
+            byte[] TextByte = EncryptAES(IvAES.getBytes("UTF-8"),
+                    KeyAES.getBytes("UTF-8"),
+                    text.getBytes("UTF-8"));
+
+            return Base64.encodeToString(TextByte, Base64.DEFAULT);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String decrypt(@NonNull final String text) {
+        try {
+            byte[] TextByte = DecryptAES(IvAES.getBytes("UTF-8"),
+                    KeyAES.getBytes("UTF-8"),
+                    Base64.decode(text.getBytes("UTF-8"), Base64.DEFAULT));
+
+            return new String(TextByte,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static byte[] EncryptAES(byte[] iv, byte[] key,byte[] text)
+    {
+        try
+        {
+            AlgorithmParameterSpec mAlgorithmParameterSpec = new IvParameterSpec(iv);
+            SecretKeySpec mSecretKeySpec = new SecretKeySpec(key, "AES");
+            Cipher mCipher = null;
+            mCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            mCipher.init(Cipher.ENCRYPT_MODE,mSecretKeySpec,mAlgorithmParameterSpec);
+
+            return mCipher.doFinal(text);
+        }
+        catch(Exception ex)
+        {
+            return null;
+        }
+    }
+
+    //AES解密，帶入byte[]型態的16位英數組合文字、32位英數組合Key、需解密文字
+    private static byte[] DecryptAES(byte[] iv,byte[] key,byte[] text)
+    {
+        try
+        {
+            AlgorithmParameterSpec mAlgorithmParameterSpec = new IvParameterSpec(iv);
+            SecretKeySpec mSecretKeySpec = new SecretKeySpec(key, "AES");
+            Cipher mCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            mCipher.init(Cipher.DECRYPT_MODE,
+                    mSecretKeySpec,
+                    mAlgorithmParameterSpec);
+
+            return mCipher.doFinal(text);
+        }
+        catch(Exception ex)
+        {
+            return null;
+        }
+    }
+
 
     /**
      * SHA加密
