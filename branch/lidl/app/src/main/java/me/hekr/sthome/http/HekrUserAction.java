587b4bcd571cf5b5f02408104bf50a4235e06e04
Ryan Hsueh <freestyle0103@gmail.com>
2019-02-25 15:53:59 +0800

    [Waht] Unbind All push notification after changeing password successfully

--- branch/lidl/app/src/main/java/me/hekr/sthome/http/HekrUserAction.java
+++ branch/lidl/app/src/main/java/me/hekr/sthome/http/HekrUserAction.java
@@ -1941,10 +1941,6 @@ public class HekrUserAction {
         pushTagBind(clientId, 0, pushTagBindListener);
     }
 
-
-
-
-
     /**
      * 4.5.19 绑定推送标签接口(此接口请在主线程调用)
      *
@@ -1998,6 +1994,28 @@ public class HekrUserAction {
             }
         });
     }
+
+    public void unbindAllPush(final HekrUser.PushTagBindListener pushTagBindListener) {
+        CharSequence url = TextUtils.concat(Constants.UrlUtil.BASE_USER_URL, SiterConstantsUtil.UrlUtil.UNBIND_ALL_PSUH_ALIAS);
+        LOG.I(TAG, "unbindAllPush > url : " + url);
+
+        deleteHekrData(url, new GetHekrDataListener() {
+
+            @Override
+            public void getSuccess(Object object) {
+                LOG.I(TAG, "unbindAllPush > success");
+                pushTagBindListener.pushTagBindSuccess();
+            }
+
+            @Override
+            public void getFail(int errorCode) {
+                LOG.I(TAG, "unbindAllPush > failed");
+                pushTagBindListener.pushTagBindFail(errorCode);
+            }
+        });
+    }
+
+
 
     /**
      * 4.5.20 解绑华为推送别名
