4a9f26f77dcef89a4e6e52a463f52e1da2021bd5
Ryan Hsueh <freestyle0103@gmail.com>
2019-09-02 18:16:02 +0800

    [What] Improved UI of THCheckDetailActivity

--- branch/lidl/app/src/main/java/me/hekr/sthome/equipment/detail/THCheckDetailActivity.java
+++ branch/lidl/app/src/main/java/me/hekr/sthome/equipment/detail/THCheckDetailActivity.java
@@ -6,13 +6,10 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
