a54e750e3a19af1bb876fcafd8139e7895980720
javofxu@github.com <skygge@yeah.net>
2020-03-03 16:25:05 +0800

    修改加密协议

--- branch/lidl/app/src/main/java/me/hekr/sthome/tools/EncryptUtil.java
+++ branch/lidl/app/src/main/java/me/hekr/sthome/tools/EncryptUtil.java
@@ -1,44 +1,21 @@
 package me.hekr.sthome.tools;
 
+
 import android.util.Base64;
-
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.AlgorithmParameterSpec;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 
 /**
  * Created by ryanhsueh on 2018/12/11
  */
 public class EncryptUtil {
 
-    // Do not modify this if unnecessary
-    private final static String IvAES = "78909876shwmwkbq" ; // 16 bytes
-    private final static String KeyAES = "322jdf9whsdiwqwq921mmekqow10k8dw"; // 32 bytes (AES 256)
-
     public static String encrypt(final String text) {
         if (text == null) {
             return "";
         }
-
-        try {
-            byte[] TextByte = EncryptAES(IvAES.getBytes("UTF-8"),
-                    KeyAES.getBytes("UTF-8"),
-                    text.getBytes("UTF-8"));
-
-            if (TextByte != null) {
-                return Base64.encodeToString(TextByte, Base64.DEFAULT);
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
+        byte[] TextByte = text.getBytes();
+        if (TextByte != null) {
+            return Base64.encodeToString(TextByte, Base64.DEFAULT);
         }
-
         return "";
     }
 
@@ -46,100 +23,12 @@ public class EncryptUtil {
         if (text == null) {
             return "";
         }
+        byte[] TextByte = Base64.decode(text, Base64.DEFAULT);
 
-        try {
-            byte[] TextByte = DecryptAES(IvAES.getBytes("UTF-8"),
-                    KeyAES.getBytes("UTF-8"),
-                    Base64.decode(text.getBytes("UTF-8"), Base64.DEFAULT));
-
-            if (TextByte != null) {
-                return new String(TextByte,"UTF-8");
-            }
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
+        if (TextByte != null) {
+            return new String(TextByte);
         }
-
         return "";
-    }
-
-    private static byte[] EncryptAES(byte[] iv, byte[] key,byte[] text)
-    {
-        try
-        {
-            AlgorithmParameterSpec mAlgorithmParameterSpec = new IvParameterSpec(iv);
-            SecretKeySpec mSecretKeySpec = new SecretKeySpec(key, "AES");
-            Cipher mCipher = null;
-            mCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            mCipher.init(Cipher.ENCRYPT_MODE,mSecretKeySpec,mAlgorithmParameterSpec);
-
-            return mCipher.doFinal(text);
-        }
-        catch(Exception ex)
-        {
-            return null;
-        }
-    }
-
-    //AES解密，帶入byte[]型態的16位英數組合文字、32位英數組合Key、需解密文字
-    private static byte[] DecryptAES(byte[] iv,byte[] key,byte[] text)
-    {
-        try
-        {
-            AlgorithmParameterSpec mAlgorithmParameterSpec = new IvParameterSpec(iv);
-            SecretKeySpec mSecretKeySpec = new SecretKeySpec(key, "AES");
-            Cipher mCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            mCipher.init(Cipher.DECRYPT_MODE,
-                    mSecretKeySpec,
-                    mAlgorithmParameterSpec);
-
-            return mCipher.doFinal(text);
-        }
-        catch(Exception ex)
-        {
-            return null;
-        }
-    }
-
-
-    /**
-     * SHA加密
-     *
-     * @param strSrc 明文
-     * @return 加密之後的密文
-     */
-    public static String shaEncrypt(String strSrc) {
-        MessageDigest md = null;
-        String strDes = null;
-        byte[] bt = strSrc.getBytes();
-        try {
-            md = MessageDigest.getInstance("SHA-256");// 將此換成SHA-1、SHA-512、SHA-384等參數
-            md.update(bt);
-            strDes = bytes2Hex(md.digest()); // to HexString
-        } catch (NoSuchAlgorithmException e) {
-            return null;
-        }
-        return strDes;
-    }
-
-    /**
-     * byte數組轉換為16進制字符串
-     *
-     * @param bts 數據源
-     * @return 16進制字符串
-     */
-    public static String bytes2Hex(byte[] bts) {
-        String des = "";
-        String tmp = null;
-        for (int i = 0; i < bts.length; i++) {
-            tmp = (Integer.toHexString(bts[i] & 0xFF));
-            if (tmp.length() == 1) {
-                des += "0";
-            }
-            des += tmp;
-        }
-        return des;
     }
 
 }
